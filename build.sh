2c1d3d6c7069c3756ac6076d32921ef458f32653
msakuta <masahiro.sakuta@gmail.com>
2020-10-19 00:17:00 +0900

    Add executable flag to build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-clang++ -g -O3 lex.cpp `llvm-config --cxxflags --ldflags --system-libs --libs all` -rdynamic -std=c++1y -o lex
+clang++ -g -O0 lex.cpp `llvm-config --cxxflags --ldflags --system-libs --libs all` -rdynamic -std=c++1y -o lex
